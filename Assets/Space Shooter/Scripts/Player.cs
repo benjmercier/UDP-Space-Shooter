8c3ed0ef5dc085fe01a9d1606dc2444d6c85f1d1
benjmercier <benjmercier11@gmail.com>
2022-06-12 03:03:57 -0400

    Created new Unity project.

---
+++ Assets/Space Shooter/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _startPos = new Vector3(0f, -3f, 0f);
+
+        [SerializeField]
+        private float _speed = 3.5f;
+
+        private float _horInput,
+            _vertInput;
+
+        private Vector3 _inputDirection;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.position = _startPos;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _horInput = Input.GetAxis("Horizontal");
+            _vertInput = Input.GetAxis("Vertical");
+
+            _inputDirection = new Vector3(_horInput, _vertInput, 0f);
+
+            transform.Translate(_inputDirection * _speed * Time.deltaTime);
+        }
+    }
+}
